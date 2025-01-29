85f9cb6dcc0036a82f8433ecf8380c88987cb009
codac-proc <codac.proc@gmail.com>
2025-01-29 21:09:07 +0900

    update to practice and exercises

---
+++ Kotlin_exercises/kotlin_exercises_task1.kt
@@ -0,0 +1,10 @@
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: String,
+    val durationInMinutes: Int
+)
+fun main() {
+    val event = Event(title="Study Kotlin", description="Commit to studying Kotlin at least 15 minutes per day.", daypart="Evening", durationInMinutes=15)
+    println(event)
+}
---
+++ Kotlin_exercises/kotlin_exercises_task2.kt
@@ -0,0 +1,13 @@
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int
+)
+enum class Daypart {
+    MORNING, AFTERNOON, EVENING
+}
+fun main() {
+    val event = Event(title="Study Kotlin", description="Commit to studying Kotlin at least 15 minutes per day.", daypart=Daypart.EVENING, durationInMinutes=15)
+    println(event)
+}
